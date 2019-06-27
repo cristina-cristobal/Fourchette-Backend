95d8709f93f4e84083db588d0cc1f364613a3304
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-06-27 17:16:31 -0400

    seeded new data

--- Fourchette-Back/app/serializers/recipe_serializer.rb
+++ Fourchette-Back/app/serializers/recipe_serializer.rb
@@ -1,4 +1,4 @@
 class RecipeSerializer < ActiveModel::Serializer
   attributes :id, :name, :image, :user_id, :copy, :prev_recipe_id, :intro, :notes, :steps
-  # has_many :ingredients
+  has_many :ingredients
 end
