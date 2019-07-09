9fc5ddcc48de151c9baca244f3a9c32efa283a02
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-07-09 08:53:21 -0400

    patch complete

--- Fourchette-Back/app/controllers/recipes_controller.rb
+++ Fourchette-Back/app/controllers/recipes_controller.rb
@@ -4,8 +4,21 @@ class RecipesController < ApplicationController
     render({json: Recipe.all})
   end
 
+  def show
+    @recipe = Recipe.find_by(id: params["id"])
+    render({json: @recipe})
+  end
+
   def create
     @recipe = Recipe.create(name: params["name"], image: params["image"], user_id: params["user_id"], copy: params["copy"], prev_recipe_id: params["prev_recipe_id"], intro: params["intro"], notes: params["notes"], steps: params["steps"])
+
+    render({json: @recipe})
+
+  end
+
+  def update
+    @recipe = Recipe.find_by(id: params["id"])
+    @recipe = Recipe.create(name: params["name"], image: params["image"], user_id: params["user_id"], copy: true, prev_recipe_id: params["prev_recipe_id"], intro: params["intro"], notes: params["notes"], steps: params["steps"])
 
     render({json: @recipe})
 
