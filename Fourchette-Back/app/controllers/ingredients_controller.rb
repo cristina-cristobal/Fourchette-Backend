f29e5982ab8e38f72155b4257de4d915a4a0f718
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-07-09 07:33:00 -0400

    creates instances of recipes and ingredients

--- Fourchette-Back/app/controllers/ingredients_controller.rb
+++ Fourchette-Back/app/controllers/ingredients_controller.rb
@@ -5,11 +5,12 @@ class IngredientsController < ApplicationController
   end
 
   def create
-
+    ing_array = []
     params["ingredients"].each do |ing|
       @ingredient = Ingredient.create(description: ing, recipe_id: params["createdRecipe"]["id"])
+      ing_array << @ingredient
     end
-
+    render ({json: ing_array})
   end
 
 end
