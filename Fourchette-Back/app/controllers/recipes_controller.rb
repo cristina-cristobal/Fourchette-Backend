d042aa45b5820c5fa29c4b3be0d2351fe9972147
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-07-10 11:34:53 -0400

    able to edit recipes

--- Fourchette-Back/app/controllers/recipes_controller.rb
+++ Fourchette-Back/app/controllers/recipes_controller.rb
@@ -24,6 +24,13 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find_by(id: params["id"])
     @recipe.update(name: params["name"], image: params["image"], user_id: params["user_id"], copy: true, prev_recipe_id: params["prev_recipe_id"], intro: params["intro"], notes: params["notes"], steps: params["steps"])
     @recipe.save
+
+    params["ingredients"].each do |ing|
+      @ingredient = Ingredient.find_by(id: ing["id"])
+      @ingredient.update(description: ing["description"])
+      @ingredient.save
+    end
+
     render({json: @recipe})
 
   end
